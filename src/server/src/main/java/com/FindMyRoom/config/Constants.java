9927dcbcf334305f628bf2adea5f7c5936dcf0fd
TranHoan <hoantxhe180030@fpt.edu.vn>
2025-04-26 20:10:27 +0700

    replace manually send email by Java Mail Sender

--- src/server/src/main/java/com/FindMyRoom/config/Constants.java
+++ src/server/src/main/java/com/FindMyRoom/config/Constants.java
@@ -1,14 +1,6 @@
 package com.FindMyRoom.config;
 
 public interface Constants {
-    interface MailProperties {
-        String HOST_NAME = "smtp.gmail.com";
-        int PORT = 465;
-        String APP_EMAIL = "hoana5k44nknd@gmail.com";
-        String APP_PASSWORD = "gmol nvte nlri ziru";
-        // get app password from 2 steps verification of Google
-    }
-
     interface QRProperties {
         String BANK_CODE = "";
         String ACCOUNT_NUMBER = "";
